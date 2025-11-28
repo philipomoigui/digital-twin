81a56e1b599abbb355c337152170dccf51d3f9ff
Philip Omoigui <philipomoigui@gmail.com>
2025-11-28 08:59:05 +0100

    Update Terraform backend for new AWS account and add GitHub OIDC setup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "twin-terraform-state-180294197745"
+    bucket         = "twin-terraform-state-353546590572"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "twin-terraform-locks"
